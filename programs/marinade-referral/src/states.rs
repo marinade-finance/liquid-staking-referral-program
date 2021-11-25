030da98dbd1b9aa3f68b6479a5e6effb70405c2a
Keisuke Wantanabe <keisukew53@gmail.com>
2021-11-25 04:53:11 -0500

    `delayed_unstake_amount` added to referral state

--- programs/marinade-referral/src/states.rs
+++ programs/marinade-referral/src/states.rs
@@ -20,21 +20,24 @@ pub struct ReferralState {
     deposit_sol_amount: u64,
     // accumulated deposit-stake-account amount (SOL, u64)
     depsoit_stake_account_amount: u64,
+
     // accumulated liquid-unstake amount (SOL, u64)
     liquid_unstake_amount: u64,
+    // accumulated count of unstake operations (u64, for stats/monitoring)
+    liquid_unstake_operations: u64,
+
     // accumulated delayed-unstake amount (SOL, u64)
-    // liquid_delayed_unstake_amount: u64,
+    delayed_unstake_amount: u64,
+    // accumulated count of delayed-unstake operations (u64, for stats/monitoring)
+    delayed_unstake_operations: u64,
 
     // Base % cut for the partner (Fee struct, basis points, default 10%)
     base_fee: Fee,
     // Max % cut for the partner (Fee struct, basis points, default 100%)
     max_fee: Fee,
-
     // Net Stake target for the max % (for example 100K SOL)
-    max_net_stake: u64,
+    max_net_stake_amount: u64,
 
-    // accumulated count of operations[4] (u64, for stats/monitoring)
-    operations: u64,
     // emergency-pause flag (bool)
     pause: bool,
 }
