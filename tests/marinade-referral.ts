c079352ca24e6bee075307cf3bb04275fc9a1e56
Keisuke Wantanabe <keisukew53@gmail.com>
2021-11-30 20:42:25 -0500

    new test script for updated PDAs

--- tests/marinade-referral.ts
+++ tests/marinade-referral.ts
@@ -28,16 +28,27 @@ describe("marinade-referral", () => {
   // benefinciary associated token address
   let beneficiaryPda: InstanceType<typeof PublicKey>;
 
+  // global state PDA
+  let globalStatePda: InstanceType<typeof PublicKey>;
+  let globalStateBump: number;
+
+  // referral state PDA
+  let referralStatePda: InstanceType<typeof PublicKey>;
+  let referralStateBump: number;
+
   // mSOL mint authority
-  const MSOL_MINT_AUTHORITY_ID = new PublicKey("3JLPCS1qM2zRw3Dp6V4hZnYHd4toMNPkNesXdX9tg6KM");
+  const MSOL_MINT_AUTHORITY_ID = new PublicKey(
+    "3JLPCS1qM2zRw3Dp6V4hZnYHd4toMNPkNesXdX9tg6KM"
+  );
   // partner name - length should be 10
   const PARTNER_NAME = "abcde12345";
   // admin account
   const ADMIN = Keypair.generate();
   // partner account
   const PARTNER = Keypair.generate();
-  // referral state account address
-  const REFERRAL = Keypair.generate();
+
+  const GLOBAL_STATE_SEED = "mrp_initialize";
+  const REFERRAL_STATE_SEED = "mrp_create_referral";
 
   before(async () => {
     // Airdrop SOLs to the admin.
@@ -67,40 +78,44 @@ describe("marinade-referral", () => {
         ASSOCIATED_TOKEN_PROGRAM_ID
       )
     )[0];
+
+    // global state PDA & bump
+    const [_global_state_pda, _global_state_bump] =
+      await PublicKey.findProgramAddress(
+        [Buffer.from(anchor.utils.bytes.utf8.encode(GLOBAL_STATE_SEED))],
+        program.programId
+      );
+    globalStatePda = _global_state_pda;
+    globalStateBump = _global_state_bump;
+
+    // referral state PDA & bump
+    const [_referral_state_pda, _referral_state_bump] =
+      await PublicKey.findProgramAddress(
+        [
+          PARTNER.publicKey.toBuffer(),
+          Buffer.from(anchor.utils.bytes.utf8.encode(REFERRAL_STATE_SEED)),
+        ],
+        program.programId
+      );
+    referralStatePda = _referral_state_pda;
+    referralStateBump = _referral_state_bump;
   });
 
-  it("should initialize referral state", async () => {
+  it("should initialize global state", async () => {
     // initialize referral account
-    await program.rpc.initialize([...Buffer.from(PARTNER_NAME)], {
+    await program.rpc.initialize(globalStateBump, {
       accounts: {
-        msolMint: msolMint.publicKey,
-        partnerAccount: PARTNER.publicKey,
-        beneficiaryAccount: beneficiaryPda,
         adminAccount: ADMIN.publicKey,
-        state: REFERRAL.publicKey,
+        globalState: globalStatePda,
         systemProgram: SystemProgram.programId,
-        associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
-        tokenProgram: TOKEN_PROGRAM_ID,
-        rent: SYSVAR_RENT_PUBKEY,
       },
-      instructions: [
-        await program.account.referralState.createInstruction(REFERRAL),
-      ],
-      signers: [REFERRAL, ADMIN],
+      signers: [ADMIN],
     });
 
-    // get referral account
-    const referralState = await program.account.referralState.fetch(
-      REFERRAL.publicKey
-    );
+    // get global account
+    const globalState = await program.account.globalState.fetch(globalStatePda);
     // check if admin address matches what we expect
-    assert.ok(referralState.adminAccount.equals(ADMIN.publicKey));
-    // check if partner mSOL ATA matches what we expect
-    assert.ok(referralState.beneficiaryAccount.equals(beneficiaryPda));
-    // check if partner name matches what we expect
-    assert.ok(
-      String.fromCharCode(...referralState.partnerName) === PARTNER_NAME
-    );
+    assert.ok(globalState.adminAccount.equals(ADMIN.publicKey));
   });
 
   it("should change authority", async () => {
@@ -111,51 +126,84 @@ describe("marinade-referral", () => {
       accounts: {
         newAdminAccount: NEW_ADMIN.publicKey,
         adminAccount: ADMIN.publicKey,
-        state: REFERRAL.publicKey,
+        globalState: globalStatePda,
       },
       signers: [ADMIN],
     });
 
     // old admin no longer has permission to change authority
-    await assert.rejects(
-      async () => {
-        await program.rpc.changeAuthority({
-          accounts: {
-            newAdminAccount: NEW_ADMIN.publicKey,
-            adminAccount: ADMIN.publicKey,
-            state: REFERRAL.publicKey,
-          },
-          signers: [PARTNER],
-        });
-      }
-    );
+    await assert.rejects(async () => {
+      await program.rpc.changeAuthority({
+        accounts: {
+          newAdminAccount: NEW_ADMIN.publicKey,
+          adminAccount: ADMIN.publicKey,
+          globalState: globalStatePda,
+        },
+        signers: [ADMIN],
+      });
+    });
 
     // update authority back to previous admin
     await program.rpc.changeAuthority({
       accounts: {
         newAdminAccount: ADMIN.publicKey,
         adminAccount: NEW_ADMIN.publicKey,
-        state: REFERRAL.publicKey,
+        globalState: globalStatePda,
       },
       signers: [NEW_ADMIN],
     });
+  });
+
+  it("should create referral PDA", async () => {
+    // create referral account
+    await program.rpc.createReferralPda(
+      referralStateBump,
+      [...Buffer.from(PARTNER_NAME)],
+      {
+        accounts: {
+          msolMint: msolMint.publicKey,
+          partnerAccount: PARTNER.publicKey,
+          beneficiaryAccount: beneficiaryPda,
+          adminAccount: ADMIN.publicKey,
+          referralState: referralStatePda,
+          globalState: globalStatePda,
+          systemProgram: SystemProgram.programId,
+          associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+          tokenProgram: TOKEN_PROGRAM_ID,
+          rent: SYSVAR_RENT_PUBKEY,
+        },
+        signers: [ADMIN],
+      }
+    );
+
+    // get referral account
+    const referralState = await program.account.referralState.fetch(
+      referralStatePda
+    );
+    // check if partner mSOL ATA matches what we expect
+    assert.ok(referralState.beneficiaryAccount.equals(beneficiaryPda));
+    // check if partner name matches what we expect
+    assert.ok(
+      String.fromCharCode(...referralState.partnerName) === PARTNER_NAME
+    );
   });
 
   it("should update referral state", async () => {
     const NEW_TRANSFER_DURATION = 2_592_000 * 2;
 
     // update referral state
-    await program.rpc.update(NEW_TRANSFER_DURATION, true, {
+    await program.rpc.updateReferral(NEW_TRANSFER_DURATION, true, {
       accounts: {
         adminAccount: ADMIN.publicKey,
-        state: REFERRAL.publicKey,
+        referralState: referralStatePda,
+        globalState: globalStatePda,
       },
       signers: [ADMIN],
     });
 
     // get referral state
     const referralState = await program.account.referralState.fetch(
-      REFERRAL.publicKey
+      referralStatePda
     );
     // check if transfer period is updated
     assert.ok(referralState.transferDuration === NEW_TRANSFER_DURATION);
