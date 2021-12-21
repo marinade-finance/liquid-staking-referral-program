0dfe67c90d10268afe32761fa9fd142a0ac9289e
luciotato <luciotato@gmail.com>
2021-12-21 06:30:31 -0300

    upgrade deploy script

--- scripts/deploy-mainnet.sh
+++ scripts/deploy-mainnet.sh
@@ -4,7 +4,7 @@ echo "deploy mainnet AFFECTING MAINNET USERS?"
 read -p "Press any key to continue..."
 echo "building VERIFIABLE .so"
 anchor build --verifiable
-solana program deploy -v -u mainnet \
+solana program deploy -v -u http://marinade.rpcpool.com \
     target/verifiable/marinade_referral.so \
     --program-id target/deploy/marinade_referral-keypair.json \
-    --upgrade-authority target/deploy/marinade_referral-keypair.json
+    --upgrade-authority ~/.config/solana/MRAX9jRu8i6Gx63t2e8XmKHSJmop4bgveTwnES8TSU3.json
