4c44f3ac57ea07aba4ee4e863b7514c82b32f5b8
Ondra Chaloupka <chalda@chainkeepers.io>
2022-09-14 18:43:38 +0200

    Test moved from separate repo and refactorings

---
+++ programs/marinade-referral/tests/src/lib.rs
@@ -0,0 +1,36 @@
+#![cfg_attr(not(debug_assertions), deny(warnings))]
+
+use marinade_finance_offchain_sdk::marinade_finance;
+use marinade_finance_offchain_sdk::solana_sdk::pubkey::Pubkey;
+use solana_program_test::{processor, ProgramTest};
+
+mod integration_test;
+
+pub mod initialize;
+
+// pub fn program_test() -> ProgramTest {
+//     ProgramTest::new(
+//         "marinade_finance",
+//         marinade_finance::ID,
+//         processor!(marinade_finance::test_entry),
+//     )
+// }
+
+pub fn find_value<T, F: FnMut() -> Option<T>>(mut gen: F) -> T {
+    loop {
+        if let Some(update) = gen() {
+            break update;
+        }
+    }
+}
+
+pub fn change_value<T: Eq, F: FnMut() -> T>(old: T, mut gen: F) -> T {
+    find_value(|| {
+        let update = gen();
+        if update != old {
+            Some(update)
+        } else {
+            None
+        }
+    })
+}
---
+++ scripts/test.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+ABSPATH=`readlink -f "$0"`
+CURRENT_DIR=`dirname "$ABSPATH"`
+
+clear
+export RUST_LOG=solana_runtime::system_instruction_processor=trace,solana_runtime::message_processor=debug,solana_bpf_loader=debug,solana_rbpf=debug
+#export RUST_LOG=solana_metrics=info,debug
+
+# +nightly does not work in current
+RUSTFLAGS=-Awarnings cargo test --manifest-path $CURRENT_DIR/../programs/marinade-referral/tests/Cargo.toml
